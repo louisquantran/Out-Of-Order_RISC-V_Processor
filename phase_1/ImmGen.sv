e45ee21d7b696ddc880e0559daa215b39e106525
louisquantran <louis.quantran@gmail.com>
2025-11-04 09:28:41 -0800

    Update naming

--- phase_1/ImmGen.sv
+++ phase_1/ImmGen.sv
@@ -21,36 +21,39 @@
 
 
 module ImmGen(
-    input logic [31:0] instruction,
+    input logic [31:0] instr,
     output logic [31:0] imm    
 );
     logic [11:0] imm_12bits;
     logic [19:0] imm_20bits;
     always_comb begin
-        automatic logic [6:0] opcode = instruction[6:0];
+        automatic logic [6:0] Opcode = instr[6:0];
         imm_12bits = 12'b0;
         imm_20bits = 20'b0;
-        imm = 32'b0;
-        case (opcode) 
+        case (Opcode) 
             // I-type, J-type, L-type
             7'b0010011, 7'b1100111, 7'b0000011: begin
-                imm_12bits = instruction[31:20];
+                imm_12bits = instr[31:20];
                 imm = {{20{imm_12bits[11]}}, imm_12bits};
             end
             // LUI
             7'b0110111: begin
-                imm_20bits = instruction[31:12];
+                imm_20bits = instr[31:12];
                 imm = {imm_20bits, {12{1'b0}}};
             end
             // BNE
             7'b1100011: begin
-                imm_12bits = {instruction[31], instruction[7], instruction[29:24], instruction[11:8]};
+                imm_12bits = {instr[31], instr[7], instr[29:24], instr[11:8]};
                 imm = {{20{imm_12bits[11]}}, imm_12bits};
             end
             // S-type
             7'b0100011: begin
-                imm_12bits = {instruction[31:25], instruction[11:7]};
+                imm_12bits = {instr[31:25], instr[11:7]};
                 imm = {{20{imm_12bits[11]}}, imm_12bits};
+            end
+            // Set immediate to 0 when no OpCode is found
+            default: begin
+                imm = 32'b0;
             end
         endcase
     end
