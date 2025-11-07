d1489c56cbc82333f536576ff93e9cde3d0b8cbc
louisquantran <louis.quantran@gmail.com>
2025-11-07 01:57:15 -0800

    Update ICache to make it combinational

--- ICache.sv
+++ ICache.sv
@@ -1,40 +1,12 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 10/31/2025 02:28:21 PM
-// Design Name: 
-// Module Name: ICache
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module ICache (
-    input logic clk,
-    input logic reset,
-    input logic [31:0] address, 
+    input  logic [31:0] address,
     output logic [31:0] instruction
 );
-    //shift address right by 2
-    logic [31:0] instr_mem[0:551];
-    
-    // initialize instruction memory
-    // initial begin
-    //      $readmemh("program.hex", mem);
-    // end
-    always_ff @(posedge clk) begin
-        if (reset) instruction <= 32'b0;
-        else instruction <= instr_mem[address>>2];
+    logic [31:0] instr_mem [0:551];
+
+    initial begin
+        $readmemh("program.mem", instr_mem);
     end
+    
+    assign instruction = instr_mem[address >> 2];
 endmodule
